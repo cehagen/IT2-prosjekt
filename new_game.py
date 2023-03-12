23d560f7d1916f48260503f2bdcb1e7994d86f06
karolinev <126757049+karolinev@users.noreply.github.com>
2023-03-12 21:48:02 +0100

    Update new_game.py

--- new_game.py
+++ new_game.py
@@ -2,15 +2,17 @@ import pygame as pg
 from pygame.locals import *
 import pickle
 
+
 pg.init() 
+
 
 global WIDTH, HEIGHT
 WIDTH = 1400
 HEIGHT = 700
 SIZE = (WIDTH, HEIGHT)
 surface = pg.display.set_mode(SIZE)
-
 font_score = pg.font.SysFont('Bauhaus 93', 30)
+
 
 FPS = 60
 clock = pg.time.Clock()
@@ -18,21 +20,23 @@ tile_size = 70
 game_over = 0 # Lager en funksjon som gjør det mulig at avataren dør
 start_menu = True # Definerer start_menu som True, slik at vi får opp startmenyen når man runner koden
 
+
 bb = pg.image.load('background.png')
 restart_img = pg.image.load('restart.png') # Hentet fra https://opengameart.org/content/ultimate-timeracer-button-pack
 start_img = pg.image.load('start.png') # Hentet fra https://opengameart.org/content/ultimate-timeracer-button-pack
 exit_img = pg.image.load('exit.png') # Hentet fra https://opengameart.org/content/ultimate-timeracer-button-pack
 #level = 1
 score = 0
-
 white = (255, 255, 255)
+
 
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     surface.blit(img, (x,y))
 
+
 def draw_grid():
-    for line in range (0, 20): # Definerer antallet kolonner med blokker vi vil dele skjermen inn i
+    for line in range (0, 20): # Definerer antallet kolonner med blokker vi vil dele inn skjermen i
         pg.draw.line(surface, (225, 255, 255), (0, line * tile_size), (WIDTH, line * tile_size))
         pg.draw.line(surface, (225, 255, 255), (line * tile_size, 0), (line * tile_size, HEIGHT))
 
@@ -45,77 +49,91 @@ class Button():
         self.rect.y = y
         self.clicked = False
         
+        
     def draw(self):
         action = False
         
-        # Tegner knappen - han tegnet den lengre nede men vet ikke om det har noe å si for framtiden
+        # Tegner knappen
         surface.blit(self.image, self.rect)
         
         # Henter musen sin posisjon
         pos = pg.mouse.get_pos()
         
-        # Sjekker om musen er over og trykker
+        # Sjekker om musen er over og trykker på knappen
         if self.rect.collidepoint(pos):
-            # Det er null siden venstre må trykkes på tror jeg, forklares ved 8.20
+            
             if pg.mouse.get_pressed()[0] == 1 and self.clicked == False:
                 action = True
                 self.clicked = True
+        
         # Null betyr at tasten er sluppet igjen
         if pg.mouse.get_pressed()[0] == 0:
             self.clicked = False
         
         return action
                 
-        
     
 class Player():
     def __init__(self, x, y):
-        self.reset(x, y)                                    # Vi kan også bruke andre navn på variabelen, så blir det større forskjell
+        self.reset(x, y)                                    
+
 
     def update(self, game_over): # Legger inn game_over som et argument i update-funksjonen, siden det er en global variabel
-        #delta x og delta yx
+        # Delta x og delta yx
         dx = 0
         dy = 0
         walk_cooldown = 10 # Bestemmer hvor fort bildene skifter
         
+        
         if game_over == 0:
             # Legger inn bevegelse for tastene
             key = pg.key.get_pressed()
+            
             if key[pg.K_SPACE] and self.jumped == False and self.in_air == False:
                 self.vel_y  = -15
                 self.jumped = True
+            
             if key[pg.K_SPACE] == False:
                 self.jumped = False
+            
             if key[pg.K_LEFT]:
                 dx -= 5
                 self.counter += 1
-                self.direction = -1                                 # Vi kan også bruke andre navn på variabelen, så blir det større forskjell
+                self.direction = -1                                 
+            
             if key[pg.K_RIGHT]:
                 dx += 5
                 self.counter += 1
-                self.direction = 1                                  # Vi kan også bruke andre navn på variabelen, så blir det større forskjell
+                self.direction = 1                                  
+            
             # Gjør at det er bildet hvor avataren står stille som vises, dersom han står stille   
             if key[pg.K_LEFT] == False and key[pg.K_RIGHT] == False:
                 self.counter = 0
                 self.index = 0
-                if self.direction == 1:                             # Vi kan også bruke andre navn på variabelen, så blir det større forskjell
+                
+                if self.direction == 1:                             
                     self.image = self.image_right[self.index]
-                if self.direction == -1:                            # Vi kan også bruke andre navn på variabelen, så blir det større forskjell
+                
+                if self.direction == -1:                            
                     self.image = self.image_left[self.index]
 
             # Animasjon
             if self.counter > walk_cooldown:
                 self.counter = 0
                 self.index += 1
+                
                 if self.index >= len(self.image_right):
                     self.index = 0
-                if self.direction == 1:                             # Vi kan også bruke andre navn på variabelen, så blir det større forskjell
+                
+                if self.direction == 1:                             
                     self.image = self.image_right[self.index]
-                if self.direction == -1:                            # Vi kan også bruke andre navn på variabelen, så blir det større forskjell
+                
+                if self.direction == -1:                           
                     self.image = self.image_left[self.index]
 
             # Legger til gravitasjonskraft
             self.vel_y += 0.75
+            
             if self.vel_y > 10:
                 self.vel_y = 10
             dy += self.vel_y
@@ -150,8 +168,6 @@ class Player():
                 game_over -= 1
                 # print(game_over) Brukte denne i starten for å se koden fungerte, og kollisjonene ble registrert
 
-
-
             self.rect.x += dx
             self.rect.y += dy
             
@@ -160,8 +176,8 @@ class Player():
             
         elif game_over == -1: # Hvis game_over = -1, betyr det at man har dødd
             self.image = self.dead_image # I såfall er det et annet bilde av avataren som vises
-            if self.rect.y > -800:                         # Kan hende vi må bytte tallet 10, men må sjekke hvordan det ser ut når vi runner
-                self.rect.y -= 5 # Avataren "flyter" opp til toppen av skjermen
+            if self.rect.y > -800:
+                self.rect.y -= 5 # Avataren "flyter" opp til toppen av skjermen og ut av bildet
 
 # Kanskje det skal skje noe annet med ham når han dør?
 # I videoen blir avataren et spøkelse, så da passer det jo fint at han "flyter" til toppen
@@ -175,11 +191,13 @@ class Player():
         
         return game_over # Returnerer en evt. ny verdi for game_over-variablen, som hvis avataren treffer lavaen
         
+        
     def reset(self, x, y):
         self.image_right = []
         self.image_left = []
         self.index = 0
         self.counter = 0
+        
         for i in range(1,3):
             img_right = pg.image.load(f'Zombie0{i}.png')
             img_right = pg.transform.scale(img_right, (100,100))
@@ -198,12 +216,12 @@ class Player():
         self.jumped = False
         self.direction = 0
         self.in_air = True # Kan ikke hoppe flere ganger på rad
-        
+       
+       
 class World():
     def __init__(self, data):
         
         self.tile_list = []
-        
 
         jordblokk = pg.image.load('dirt_tile_08.png')
         gress = pg.image.load('dirt_tile_06.png')
@@ -211,6 +229,7 @@ class World():
         row_count = 0
         for row in data:
             col_count = 0
+            
             for tile in row:
                 if tile == 1:
                     img = pg.transform.scale(jordblokk, (tile_size, tile_size))
@@ -219,6 +238,7 @@ class World():
                     img_rect.y = row_count*tile_size
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
+                
                 if tile == 2:
                     img = pg.transform.scale(gress, (tile_size, tile_size))
                     img_rect = img.get_rect()
@@ -226,17 +246,21 @@ class World():
                     img_rect.y = row_count*tile_size
                     tile = (img, img_rect)
                     self.tile_list.append(tile)
+                
                 if tile == 3:
                     lava = Lava(col_count * tile_size, row_count * tile_size + (tile_size//2)) # Pluss for at den skal ligge på bunnen
                     lava_group.add(lava)
+                
                 if tile == 4:
                     exit = Exit(col_count * tile_size, row_count * tile_size - (tile_size))
                     exit_group.add(exit)
+                
                 if tile == 5:
                     coin = Coin(col_count * tile_size + (tile_size // 2), row_count * tile_size + (tile_size//2)) 
                     coin_group.add(coin)
                 col_count += 1
             row_count += 1
+            
             
     def draw(self):
         for tile in self.tile_list:
@@ -253,6 +277,7 @@ class Lava(pg.sprite.Sprite):
         self.rect.x = x
         self.rect.y = y
         
+        
 class Coin(pg.sprite.Sprite):
     def __init__(self,x,y):
         pg.sprite.Sprite.__init__(self) # Legger til objekter i spill
@@ -260,6 +285,7 @@ class Coin(pg.sprite.Sprite):
         self.image = pg.transform.scale(img, (tile_size//2, tile_size//2))
         self.rect = self.image.get_rect()
         self.rect.center = (x,y) #gjør at vi posisjonerer fra midtpunktet av mynten
+
 
 class Exit(pg.sprite.Sprite):
     def __init__(self,x,y):
@@ -269,6 +295,7 @@ class Exit(pg.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+
 
 # 0 = tom
 # 1 = dirt-kloss
@@ -296,16 +323,19 @@ lava_group = pg.sprite.Group()
 coin_group = pg.sprite.Group()
 exit_group = pg.sprite.Group()
 
-#Vi kan bruke dette4 senere når vi skal ha flere leveler
-#henter data fra mappen med leveler
-#pickle_in = open(caroline1_data, 'rb')
-#world_data = pickle.load(pickle_in)
+
+# Vi kan bruke dette senere når vi skal lage flere leveler
+# Henter data fra mappen med leveler
+# Pickle_in = open(caroline1_data, 'rb')
+# World_data = pickle.load(pickle_in)
 world = World(world_data) 
+
 
 # Lager knappene
 restart_button = Button(WIDTH //2 - 200, HEIGHT // 4 , restart_img)
 start_button = Button(WIDTH //2 - 550, HEIGHT // 4, start_img)
 exit_button = Button(WIDTH //2 + 250, HEIGHT // 4, exit_img)
+
 
 run = True
 while run == True:
@@ -315,8 +345,10 @@ while run == True:
     surface.blit(bb, (0, 0))
     
     if start_menu == True:
+        
         if exit_button.draw() == True:
             run = False
+        
         if start_button.draw() == True:
             start_menu = False
     
@@ -327,8 +359,8 @@ while run == True:
         coin_group.draw(surface)
         exit_group.draw(surface)
         
-        if game_over == 0:                                    # Trenger foreløpig ikke denne delen
-            #blob_group.update()                               # Dette vil gjøre at blobbene slutter å bevege seg når avataren treffer lavaen
+        if game_over == 0:                                                                 
+            
             #sjekker om en mynt har blitt tatt
             if pg.sprite.spritecollide(player, coin_group, True):
                 score += 1
@@ -338,8 +370,10 @@ while run == True:
         
         # Hvis avataren dør
         if game_over == -1:
+            
             if exit_button.draw() == True: # Tegner exit-knappen for å kunne avslutte spillet
                 run = False # Hvis knappen trykkes på, endres run-variablen til False, og spillet avsluttes
+            
             if restart_button.draw() == True: # Tegner reset-knappen så man kan starte spillet på nytt
                 player.reset(100, HEIGHT - 300)
                 game_over = 0 # Endrer game_over-variablen til 0 igjen, så spillet kjøres fra begynnelsen
@@ -363,8 +397,10 @@ while run == True:
     #print(world.tile_list)
     
     for event in pg.event.get():
+        
         if event.type == pg.QUIT:
             run = False
     pg.display.update()
-            
+    
+    
 pg.quit()
